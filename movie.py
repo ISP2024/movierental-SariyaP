f602f4606b11a73e9042884ab0c1c77ccac221f1
Sariya Punprang <sariya.pu@ku.th>
2024-10-20 21:28:16 +0700

    Define a Factory for Movies (MovieCatalog)

--- movie.py
+++ movie.py
@@ -1,3 +1,4 @@
+import csv
 from dataclasses import dataclass
 
 from pricing import *
@@ -10,7 +11,7 @@ class Movie:
 
     @dataclass(frozen=True)
     def __init__(self, title: str, year: int, genre: list):
-        # Initialize a new movie. 
+        # Initialize a new movie.
         self.__title = title
         self.__year = year
         self.__genre = genre
@@ -19,7 +20,29 @@ class Movie:
         return self.__title
 
     def is_genre(self, genre):
-        return True if genre in self.__genre else False
+        return genre in self.__genre
 
     def __str__(self):
         return f"{self.__title} ({self.__year})"
+
+    def get_year(self):
+        return self.__year
+
+class MovieCatalog(ABC):
+    def __init__(self):
+        self.__movies = []
+        with open('movies.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for row in reader:
+                movie_id, title, year, genres_str = row
+                genres = genres_str.split('|')
+                movie = Movie(title, int(year), genres)
+                self.__movies.append(movie)
+
+    def get_movie(self, title, year=None):
+        for movie in self.__movies:
+            if movie.get_title() == title:
+                if year is None or movie.get_year() == year:
+                    return movie
+        return None
+
