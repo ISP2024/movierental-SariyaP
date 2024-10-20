71616c0daf54a31c6c73dad311d205ec8f5f9e63
Sariya Punprang <sariya.pu@ku.th>
2024-10-20 21:01:41 +0700

    Add Attributes to Movie

--- movie.py
+++ movie.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from pricing import *
 
 
@@ -5,14 +7,19 @@ class Movie:
     """
     A movie available for rent.
     """
-    
-    def __init__(self, title, strategy: PriceStrategy):
+
+    @dataclass(frozen=True)
+    def __init__(self, title: str, year: int, genre: list):
         # Initialize a new movie. 
-        self.title = title
-        self.strategy = strategy
+        self.__title = title
+        self.__year = year
+        self.__genre = genre
 
     def get_title(self):
-        return self.title
-    
+        return self.__title
+
+    def is_genre(self, genre):
+        return True if genre in self.__genre else False
+
     def __str__(self):
-        return self.title
+        return f"{self.__title} ({self.__year})"
